85cce90d0a396cd7aeadaa3faab8d9b7ea722294
MFarisA <muhfarisassami@gmail.com>
2025-06-13 01:48:16 +0700

    Refactor carousel and navbar components for improved UI styling and code readability.

--- resources/views/livewire/components/carousel.blade.php
+++ resources/views/livewire/components/carousel.blade.php
@@ -1,3 +1,3 @@
-<div class="max-w-screen-xl mx-auto">
-    <img src="{{asset('asset/carousel.png')}}">
+<div class="max-w-screen-xl mx-auto m-6">
+    <img src="{{asset('asset/carousel1.png')}}" class="rounded-2xl">
 </div>
--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -1,33 +1,38 @@
 <nav class="bg-red-50 mx-auto px-4">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen p-2">
-        <a href={{route('home')}} wire:navigate>
-            <img src="{{asset('asset/logos/SHOPPER-BRAND.svg')}}" class="items-center">
-        </a>
+    <div class="flex justify-between items-center mx-auto max-w-screen p-2 gap-x-4">
 
-        <div class="flex items-center">
-            <span>categories</span>
+        <div class="flex items-center gap-x-3">
+            <a href="{{ route('home') }}" wire:navigate class="flex items-center gap-x-1">
+                <img src="{{ asset('asset/logos/shopper.svg') }}" class="h-8" alt="Shopper Brand Logo">
+                <label class="font-poppins font-medium text-[20px] text-[#0A8048] -ml-1">SHOPPER</label>
+            </a>
+            <div class="flex items-center">
+                <span class="text-sm font-medium whitespace-nowrap">Categories</span>
+            </div>
         </div>
-            <form class="w-full max-w-xl flex-1 mx-auto bg-red-50">
-                <form class="w-full max-w-xl mx-auto bg-white flex-1">
 
-                <label class="text-black sr-only">search</label>
+        <div class="flex-1 max-w-xl">
+            <form class="w-full">
+                <label for="search-input" class="text-black sr-only">search</label>
                 <div class="relative">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <x-eva-search-outline class="w-4 h-4 text-gray-500"/>
                     </div>
-                    <input type="search" placeholder="search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 ">
+                    <input type="search" id="search-input" placeholder="search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50">
                 </div>
             </form>
-        <div class="flex items-center mx-9 gap-6">
-            <x-untitledui-mail class="h-6 w-6 cursor-pointer"/>
-            <x-heroicon-o-shopping-cart class="h-6 w-6 cursor-pointer"/>
-{{--            <x-heroicon-o-bell class="h-6 w-6 cursor-pointer"/>--}}
         </div>
 
-        <div class="flex items-center gap-4">
-            <div class="flex items-center gap-1 cursor-pointer">
-                <img src="{{asset('asset/pluto.png')}}" class="relative rounded-full size-10">
+        <div class="flex items-center gap-x-3">
+            <div class="flex items-center gap-x-3">
+                <x-untitledui-mail class="h-6 w-6 cursor-pointer"/>
+                <x-heroicon-o-shopping-cart class="h-6 w-6 cursor-pointer"/>
+                {{-- <x-heroicon-o-bell class="h-6 w-6 cursor-pointer"/> --}}
+            </div>
+            <div class="flex items-center">
+                <img src="{{ asset('asset/pluto.png') }}" class="relative rounded-full size-10 cursor-pointer">
             </div>
         </div>
+
     </div>
 </nav>
