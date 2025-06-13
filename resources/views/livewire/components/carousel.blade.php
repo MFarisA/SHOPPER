8eff827ae85896ad0837a28ab68333e17e0dc2f1
MFarisA <muhfarisassami@gmail.com>
2025-06-14 01:44:37 +0700

    Enhance carousel with navigation buttons and update main layout with filter options and section header

--- resources/views/livewire/components/carousel.blade.php
+++ resources/views/livewire/components/carousel.blade.php
@@ -1,8 +1,28 @@
-<div class="relative max-w-screen-xl mx-auto m-6 rounded-2xl overflow-hidden aspect-[1280/266]">
-    <img src="{{asset('asset/carousel2.png')}}" class="w-full h-full object-cover">
+<div class="relative max-w-screen-xl mx-auto m-6 rounded-2xl overflow-hidden aspect-[1280/266] group">
+    <img src="{{asset('asset/carousel1.png')}}" class="w-full h-full object-cover">
     <div class="absolute bottom-5 left-5 flex items-center gap-x-2">
         <button class="w-2.5 h-2.5 rounded-full bg-gray-800" aria-label="Go to slide 1"></button>
         <button class="w-2.5 h-2.5 rounded-full bg-gray-300 hover:bg-gray-400" aria-label="Go to slide 2"></button>
         <button class="w-2.5 h-2.5 rounded-full bg-gray-300 hover:bg-gray-400" aria-label="Go to slide 3"></button>
     </div>
+    <button type="button"
+            class="hs-carousel-prev hs-carousel-disabled:opacity-50 hs-carousel-disabled:cursor-default absolute top-1/2 start-2 inline-flex justify-center items-center size-10 bg-white border border-gray-100 text-gray-800 rounded-full shadow-2xs hover:bg-gray-100 -translate-y-1/2 focus:outline-hidden opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition">
+    <span class="text-2xl" aria-hidden="true">
+      <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+           fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m15 18-6-6 6-6"></path>
+      </svg>
+    </span>
+        <span class="sr-only">Previous</span>
+    </button>
+    <button type="button"
+            class="hs-carousel-next hs-carousel-disabled:opacity-50 hs-carousel-disabled:cursor-default absolute top-1/2 end-2 inline-flex justify-center items-center size-10 bg-white border border-gray-100 text-gray-800 rounded-full shadow-2xs hover:bg-gray-100 -translate-y-1/2 focus:outline-hidden opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition">
+        <span class="sr-only">Next</span>
+        <span class="text-2xl" aria-hidden="true">
+      <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+           fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m9 18 6-6-6-6"></path>
+      </svg>
+    </span>
+    </button>
 </div>
--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -1,3 +1,45 @@
-<div>
-    <livewire:components.carousel />
+<div class="">
+    <div>
+        <livewire:components.carousel/>
+    </div>
+    <div class="max-w-screen-xl mx-auto">
+        <div class="flex flex-wrap gap-x-4 items-center">
+            <div class="bg-[#EBEDEC] px-5 py-2 rounded-full text-gray-700 hover:bg-gray-300 transition hover:text-black">
+                <button class="cursor-pointer">
+                    <span class="font-poppins text-sm font-medium ">For User</span>
+                </button>
+            </div>
+            <div class="bg-[#EBEDEC] px-5 py-2 rounded-full text-gray-700 hover:bg-gray-300 transition hover:text-black">
+                <button class="cursor-pointer">
+                    <span class="font-poppins text-gray-700 text-sm font-medium">Electronic</span>
+                </button>
+            </div>
+            <div class="bg-[#EBEDEC] px-5 py-2 rounded-full text-gray-700 hover:bg-gray-300 transition hover:text-black">
+                <button class="cursor-pointer">
+                    <span class="font-poppins text-gray-700 text-sm font-medium">Fashion</span>
+                </button>
+            </div>
+            <div class="bg-[#EBEDEC] px-5 py-2 rounded-full text-gray-700 hover:bg-gray-300 transition hover:text-black">
+                <button class="cursor-pointer flex items-center gap-x-2">
+                    <span class="font-poppins text-gray-700 text-sm font-medium">Offer</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </button>
+            </div>
+            <div class="bg-[#EBEDEC] px-5 py-2 rounded-full text-gray-700 hover:bg-gray-300 transition hover:text-black">
+                <button class="cursor-pointer flex items-center gap-x-2">
+                    <span class="font-poppins text-gray-700 text-sm font-medium">All Filter</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="mt-6 max-w-screen-xl mx-auto">
+        <span class="font-poppins text-2xl font-medium">
+            Headphone For You
+        </span>
+    </div>
 </div>
