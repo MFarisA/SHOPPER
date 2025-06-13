be13093b8a1f4b8f7981ce6cf97f13d5425ca021
MFarisA <muhfarisassami@gmail.com>
2025-06-14 02:03:15 +0700

    Remove commented-out unused route for cleaner code in web routes file.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Livewire\Main;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('components.layouts.app');
-// });
-
 Route::get('/', Main::class)->name('home');
